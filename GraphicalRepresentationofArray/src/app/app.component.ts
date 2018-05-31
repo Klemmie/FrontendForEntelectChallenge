bb4068779e6c1c8e9cb87bb9aabc5edc893fe02d
Ruaan-HOME <ruaanmk@gmail.com>
2018-05-31 18:44:19 +0200

    Interim website

--- GraphicalRepresentationofArray/src/app/app.component.ts
+++ GraphicalRepresentationofArray/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +10,18 @@ export class AppComponent {
   interpreted = [];
   interpretedRound = [];
   playerData = [];
-  start = 0;
-  end = 0;
+  start = -1;
+  roundStart = -1;
+  end = -1;
+  roundEnd = -1;
   playerOneHealth;
   playerTwoHealth;
   playerOneEnergy;
   playerTwoEnergy;
   playerOneScore;
   playerTwoScore;
+  playerOneName;
+  playerTwoName;
   uploaded = false;
   cell = [];
   images = [];
@@ -30,86 +34,109 @@ export class AppComponent {
 
   fileUpload(event) {
     this.uploaded = true;
-    let reader = new FileReader();
+    const reader = new FileReader();
     reader.readAsText(event.srcElement.files[0]);
-    let me = this;
+    const me = this;
     reader.onload = function () {
       me.fileText = reader.result;
     }
   }
 
-  loopText(){
-    this.interpreted = this.fileText.split("\n");
+  loopText() {
+    this.interpreted = this.fileText.split('\n');
+    this.start = this.roundStart;
+    this.roundEnd = this.roundStart + 1;
+    this.end = this.roundEnd;
     for (let i = 0; i < this.interpreted.length; i++) {
-      if (this.interpreted[i] === "Starting round 0"){
+      if (this.interpreted[i] === ('Starting round ' + this.start).toString() ) {
         this.start = i;
-      } else if (this.interpreted[i] === "Starting round 1"){
+      } else if (this.interpreted[i] === ('Starting round ' + this.end).toString() ) {
         this.end = i;
       }
     }
+
     for (let i = this.start + 5; i < this.end - 1; i++) {
       this.interpretedRound.push(this.interpreted[i]);
     }
 
-    this.playerData = this.interpretedRound[1].split(" ");
-    this.playerOneHealth = this.playerData[2].split("=")[1].replace(',','');
-    this.playerOneEnergy = this.playerData[3].split("=")[1].replace(',','');
-    this.playerOneScore = this.playerData[4].split("=")[1].replace(',','');
+    if (this.end !== -1) {
+      this.playerData = this.interpretedRound[1].split(' ');
+      this.playerOneName = this.playerData[1];
+      this.playerOneHealth = this.playerData[2].split('=')[1].replace(',', '');
+      this.playerOneEnergy = this.playerData[3].split('=')[1].replace(',', '');
+      this.playerOneScore = this.playerData[4].split('=')[1].replace(',', '');
 
-    this.playerData = this.interpretedRound[2].split(" ");
-    this.playerTwoHealth = this.playerData[2].split("=")[1].replace(',','');
-    this.playerTwoEnergy = this.playerData[3].split("=")[1].replace(',','');
-    this.playerTwoScore = this.playerData[4].split("=")[1].replace(',','');
+      this.playerData = this.interpretedRound[2].split(' ');
+      this.playerTwoName = this.playerData[1];
+      this.playerTwoHealth = this.playerData[2].split('=')[1].replace(',', '');
+      this.playerTwoEnergy = this.playerData[3].split('=')[1].replace(',', '');
+      this.playerTwoScore = this.playerData[4].split('=')[1].replace(',', '');
 
-    let j = 0;
-    for (let i = 3; i < this.interpretedRound.length - 1; i++) {
-      this.interpretedRound[i] = this.interpretedRound[i].replace(new RegExp(']\\[', 'g'), '],[');
-      this.cell[j] = this.interpretedRound[i].split(',');
-      j++;
+      let j = 0;
+      for (let i = 3; i < this.interpretedRound.length - 1; i++) {
+        this.interpretedRound[i] = this.interpretedRound[i].replace(new RegExp(']\\[', 'g'), '],[');
+        this.cell[j] = this.interpretedRound[i].split(',');
+        j++;
+      }
+      this.drawMethod();
     }
-    this.drawMethod();
   }
 
-  drawMethod(){
+  drawMethod() {
     for (this.rows = 0; this.rows < this.cell.length; this.rows++) {
       for (this.columns = 0; this.columns < this.cell[this.rows].length; this.columns++) {
-        if(this.cell[this.rows][this.columns] === "E"){
-          this.images.push("assets/energyDONE.jpg");
-        } else if (this.cell[this.rows][this.columns] === "D"){
-          this.images.push("assets/defenseDONE.jpg");
-        } else if ((this.cell[this.rows][this.columns] === "A") && (this.rows <= 3)){
-          this.images.push("assets/attackTowerPLA.jpg");
-        } else if ((this.cell[this.rows][this.columns] === "A") && (this.rows > 3)){
-          this.images.push("assets/attackTowerOPP.jpg");
-        } else if (this.cell[this.rows][this.columns] === ">"){
-          this.images.push("assets/missilePLA.jpg");
-        } else if (this.cell[this.rows][this.columns] === "<"){
-          this.images.push("assets/missileOPP.jpg");
-        } else if ((this.cell[this.rows][this.columns] !== '[') && (this.cell[this.rows][this.columns] !== ']')){
-          this.images.push("assets/empty.jpg");
-        } else if ((this.cell[this.rows][this.columns]).contains("e")){
-          this.images.push("assets/energyDONE.jpg");
-        } else if (this.cell[this.rows][this.columns] === "d"){
-          this.images.push("assets/defenseDONE.jpg");
-        } else if ((this.cell[this.rows][this.columns] === "a") && (this.rows <= 3)){
-          this.images.push("assets/attackTowerPLA.jpg");
-        } else if ((this.cell[this.rows][this.columns] === "a") && (this.rows > 3)){
-          this.images.push("assets/attackTowerOPP.jpg");
+        let flag = 0;
+        this.images.push('assets/edge.png');
+        if ((this.cell[this.rows][this.columns]).includes('<')) {
+          this.images.push('assets/missileOPP.jpg');
+        } else if ((this.cell[this.rows][this.columns].includes('E')) && (flag === 0)) {
+          this.images.push('assets/energyDONE.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('D') && (flag === 0)) {
+          this.images.push('assets/defenseDONE.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('A') && (this.columns <= 3) && (flag === 0)) {
+          this.images.push('assets/attackDonePLA.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('A') && (this.columns > 3) && (flag === 0)) {
+          this.images.push('assets/attackDoneOPP.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('e') && (flag === 0)) {
+          this.images.push('assets/energyBUSY.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('d') && (flag === 0)) {
+          this.images.push('assets/defenseBUSY.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('a') && (this.columns <= 3) && (flag === 0)) {
+          this.images.push('assets/attackBusyPLA.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('a') && (this.columns > 3) && (flag === 0)) {
+          this.images.push('assets/attackBusyOPP.jpg');
+          flag = 1;
+        } else if (flag === 0) {
+          this.images.push('assets/empty.jpg');
+          flag = 1;
+        } else if ((this.cell[this.rows][this.columns]).includes('>')) {
+          this.images.push('assets/missilePLA.jpg');
         }
       }
+      this.images.push('assets/edge.png');
     }
 
     let j = 0;
-    for (let i = 0; i < 8; i++, j++) {
+    for (let i = 0; i < 17; i++, j++) {
       this.imageRow1[j] = this.images[i];
     }
-    for (let i = 8, j = 0; i < 16; i++, j++) {
+    j = 0;
+    for (let i = 17; i < 34; i++, j++) {
       this.imageRow2[j] = this.images[i];
     }
-    for (let i = 16, j = 0; i < 24; i++, j++) {
+    j = 0;
+    for (let i = 34; i < 51; i++, j++) {
       this.imageRow3[j] = this.images[i];
     }
-    for (let i = 24, j = 0; i < 32; i++, j++) {
+    j = 0;
+    for (let i = 51; i < 68; i++, j++) {
       this.imageRow4[j] = this.images[i];
     }
   }
